d7e9e5e0fa725ba166395bd8215a746d3e01eadb
MarcoPicheca <marco.picheca@gmail.com>
2025-05-23 01:05:15 +0200

    date finder to be done - input parser done

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,6 +1,17 @@
-#include <iostream>
-#include <fstream>
+#include "Bitcoin.hpp"
 
-int main() {
-    return 0;
+int main(int ac, char *av[])
+{
+	if (ac != 2)
+	{
+		std::cerr << "Error: could not open file.";
+		return (1);
+	}
+	Bitcoin ciao;
+	ciao.csvParser();
+	std::string arg;
+	arg.append(av[1]);
+	ciao.inputParser(arg);
+	
+	return 0;
 }
