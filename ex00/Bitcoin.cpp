0b653e94f0500d03459a87d07a6cf84d884cc678
MarcoPicheca <marco.picheca@gmail.com>
2025-05-23 20:31:03 +0200

    ex00 last checkes to be done

---
+++ ex00/Bitcoin.cpp
@@ -0,0 +1,185 @@
+#include "Bitcoin.hpp"
+
+
+Bitcoin::Bitcoin(const Bitcoin& copy)
+{
+	_valuesCsv = copy._valuesCsv;
+	_yearCsv = copy._yearCsv;
+	_monthCsv = copy._monthCsv;
+	_dayCsv = copy._dayCsv;
+	_dateCsv = copy._dateCsv;
+	_valuesInput = copy._valuesInput;
+	_yearInput = copy._yearInput;
+	_monthInput = copy._monthInput;
+	_dayInput = copy._dayInput;
+	_dateInput = copy._dateInput;
+	result = copy.result;
+}
+
+Bitcoin& Bitcoin::operator=(const Bitcoin& copy)
+{
+	if (this != &copy)
+	{
+		_valuesCsv = copy._valuesCsv;
+		_yearCsv = copy._yearCsv;
+		_monthCsv = copy._monthCsv;
+		_dayCsv = copy._dayCsv;
+		_dateCsv = copy._dateCsv;
+		_valuesInput = copy._valuesInput;
+		_yearInput = copy._yearInput;
+		_monthInput = copy._monthInput;
+		_dayInput = copy._dayInput;
+		_dateInput = copy._dateInput;
+		result = copy.result;
+	}
+	return *this;
+}
+
+void	Bitcoin::csvParser()
+{
+	std::ifstream file("./data.csv");
+	std::string line;
+	
+	std::getline(file, line);
+	while (std::getline(file, line))
+	{
+		std::stringstream ss(line);
+		std::string value_str;
+		std::string date;
+		if (std::getline(ss, date, ',') && std::getline(ss, value_str, ','))
+		{
+			_valuesCsv.push_back(std::atof(value_str.c_str()));
+			std::stringstream ss2(date);
+			_dateCsv.push_back(date);
+			std::string year;
+			std::string month;
+			std::string day;
+			std::getline(ss2, year, '-');
+			std::getline(ss2, month, '-');
+			std::getline(ss2, day, '-');
+			_yearCsv.push_back(std::atoi(year.c_str()));
+			_monthCsv.push_back(std::atoi(month.c_str()));
+			_dayCsv.push_back(std::atoi(day.c_str()));
+		}
+	}
+}
+
+void Bitcoin::inputError(const char * str, int flag)
+{
+	std::cout << str;
+	if (flag == 0)
+		std::cout << std::endl;
+	_yearInput.push_back(-1);
+	_monthInput.push_back(-1);
+	_dayInput.push_back(-1);
+	_valuesInput.push_back(-1);
+}
+
+int Bitcoin::findClosestDate(unsigned int y, unsigned int m, unsigned int d)
+{
+	int index = 0;
+	while (_yearCsv[index] != y)
+		index++;
+	while (_monthCsv[index] != m)
+		index++;
+	unsigned int range_1 = index;
+	unsigned int range_2 = 0;
+	while (_monthCsv[index] == m)
+		index++;
+	range_2 = index;
+	while (_dayCsv[range_1] != d && _dayCsv[range_1] <= d && range_1 < range_2)
+		range_1++;
+	if (d == _dayCsv[range_1])
+		return range_1;
+	if (d + 1 == _dayCsv[range_1])
+		return range_1;
+	else
+		return range_1 - 1;
+	return 0;
+}
+
+int Bitcoin::inputParser(std::string& arg)
+{
+	std::ifstream file(arg.c_str());
+	std::string line;
+	int i = -1;
+	int ind = -1;
+	std::string year;
+	std::string month;
+	std::string day;
+	std::string value_str;
+	std::string date;
+	unsigned int y = 0;
+	unsigned int m = 0;
+	unsigned int d = 0;
+
+	std::getline(file, line);
+	while (std::getline(file, line))
+	{
+		++i;
+		std::stringstream ss(line);
+		if (std::getline(ss, date, '|') && std::getline(ss, value_str, '|'))
+		{
+			double val = std::atof(value_str.c_str());
+			if (val < 0)
+			{
+				inputError("Error: not a positive number.", 0);
+				continue;
+			}
+			else if (val > 100)
+			{
+				inputError("Error: too large number.", 0);
+				continue;
+			}
+			_valuesInput.push_back(val);
+			std::stringstream ss2(date);
+			_dateInput.push_back(date);
+			std::getline(ss2, year, '-');
+			std::getline(ss2, month, '-');
+			std::getline(ss2, day, ' ');
+			if (year.find_first_not_of("0123456789") != std::string::npos
+				|| month.find_first_not_of("0123456789") != std::string::npos
+				|| day.find_first_not_of("0123456789") != std::string::npos)
+			{
+				inputError("Error: non numerical input.", 0);
+				continue;
+			}
+			y = std::atoi(year.c_str());
+			m = std::atoi(month.c_str());
+			d = std::atoi(day.c_str());
+			if (y > 2022 || y < 2009 || m < 1 || m > 12 || d < 1 || d > 31
+				|| (m == 2 && d > 29) || (y != 2012 && y != 2016 && y != 2020 && d == 29))
+			{
+				inputError("Error: bad input => ", 1);
+				std::cout << y << '-' << m << '-' << d << std::endl;
+				continue ;
+			}
+			_yearInput.push_back(y);
+			_monthInput.push_back(m);
+			_dayInput.push_back(d);
+			ind = findClosestDate(y, m, d);
+			result = _valuesInput[i] * _valuesCsv[ind];
+			std::cout << _dateCsv[ind];
+			std::cout << " => " << _valuesInput[i];
+			std::cout << " = " <<  result << std::endl;
+		}
+		else if (!date.empty() && value_str.empty())
+        {
+			inputError("Error: bad input => ", 1);
+			std::cout << date << std::endl;
+        }
+		else
+        {
+			inputError("Error: bad input", 0);
+        }
+	}
+	return 0;
+}
+
+Bitcoin::Bitcoin()
+{
+}
+
+Bitcoin::~Bitcoin()
+{
+}
--- ex00/Bitcoin.hpp
+++ ex00/Bitcoin.hpp
@@ -21,128 +21,16 @@ class Bitcoin
 		std::vector<unsigned int> _monthInput;
 		std::vector<unsigned int> _dayInput;
 		std::vector<std::string> _dateInput;
+		double result;
 	public:
 		Bitcoin();
-		// Bitcoin(const Bitcoin& copy);
-		// Bitcoin& operator=(const Bitcoin& copy);
-		void csvParser();
-		int inputParser(std::string& arg);
-		void inputError(const char * str, int flag);
+		Bitcoin(const Bitcoin& copy);
+		Bitcoin& operator=(const Bitcoin& copy);
+		void	csvParser();
+		int		inputParser(std::string& arg);
+		void	inputError(const char * str, int flag);
+		int		findClosestDate(unsigned int y, unsigned int m, unsigned int d);
 		~Bitcoin();
 };
-
-void	Bitcoin::csvParser()
-{
-	std::ifstream file("./data.csv");
-	std::string line;
-	// size_t	i = 0;
-	
-	std::getline(file, line);
-	while (std::getline(file, line))
-	{
-		std::stringstream ss(line);
-		std::string value_str;
-		std::string date;
-		if (std::getline(ss, date, ',') && std::getline(ss, value_str, ','))
-		{
-			_valuesCsv.push_back(std::atof(value_str.c_str()));
-			std::stringstream ss2(date);
-			_dateCsv.push_back(date);
-			std::string year;
-			std::string month;
-			std::string day;
-			std::getline(ss2, year, '-');
-			std::getline(ss2, month, '-');
-			std::getline(ss2, day, '-');
-			_yearCsv.push_back(std::atoi(year.c_str()));
-			_monthCsv.push_back(std::atoi(month.c_str()));
-			_dayCsv.push_back(std::atoi(day.c_str()));
-			// std::cout << _yearCsv[i] << ' ' << _monthCsv[i] << ' ' << _dayCsv[i] << std::endl;
-		}
-		// i++;
-	}
-}
-
-void Bitcoin::inputError(const char * str, int flag)
-{
-	std::cout << str;
-	if (flag == 0)
-		std::cout << std::endl;
-	_yearInput.push_back(-1);
-	_monthInput.push_back(-1);
-	_dayInput.push_back(-1);
-	_valuesInput.push_back(-1);
-}
-
-int Bitcoin::inputParser(std::string& arg)
-{
-	std::ifstream file(arg.c_str());
-	std::string line;
-	int i = -1;
-	std::string year;
-	std::string month;
-	std::string day;
-	std::string value_str;
-	std::string date;
-
-	std::getline(file, line);
-	while (std::getline(file, line))
-	{
-		++i;
-		std::stringstream ss(line);
-		if (std::getline(ss, date, '|') && std::getline(ss, value_str, '|'))
-		{
-			double val = std::atof(value_str.c_str());
-			if (val < 0)
-			{
-				inputError("Error: not a positive number.", 0);
-				continue;
-			}
-			else if (val > 100)
-			{
-				inputError("Error: too large number.", 0);
-				continue;
-			}
-			_valuesInput.push_back(val);
-			std::stringstream ss2(date);
-			_dateInput.push_back(date);
-			std::getline(ss2, year, '-');
-			std::getline(ss2, month, '-');
-			std::getline(ss2, day, ' ');
-			if (year.find_first_not_of("0123456789") != std::string::npos
-				|| month.find_first_not_of("0123456789") != std::string::npos
-				|| day.find_first_not_of("0123456789") != std::string::npos)
-			{
-				inputError("Error: non numerical input.", 0);
-				continue;
-			}
-			int y = std::atoi(year.c_str());
-			int m = std::atoi(month.c_str());
-			int d = std::atoi(day.c_str());
-			if (y > 2022 || y < 2009 || m < 1 || m > 12 || d < 1 || d > 31
-				|| (m == 2 && d > 29) || (y != 2012 && y != 2016 && y != 2020 && d == 29))
-			{
-				inputError("Error: bad input => ", 1);
-				std::cout << y << '-' << m << '-' << d;
-				continue ;
-			}
-			_yearInput.push_back(y);
-			_monthInput.push_back(m);
-			_dayInput.push_back(d);
-			std::cout << _yearInput[i] << ' ' << _monthInput[i] << ' ' << _dayInput[i] << ' '  << _valuesInput[i] << " valore di i = " << i << std::endl;
-		}
-		else
-			inputError("Error: could't find a value.", 0);
-	}
-	return 0;
-}
-
-Bitcoin::Bitcoin()
-{
-}
-
-Bitcoin::~Bitcoin()
-{
-}
 
 #endif
