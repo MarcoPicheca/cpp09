fef06a99b387be45190f614fcd9d002daeb5bbf9
MarcoPicheca <marco.picheca@gmail.com>
2025-06-11 12:58:50 +0200

    gen jacobsthal sequence done; started adding from min list

--- ex02/PmergeMe.hpp
+++ ex02/PmergeMe.hpp
@@ -14,27 +14,73 @@ class PmergeMe
 {
 	private:
 		std::deque<T> tokens;
+		std::deque<T> jacobSthal;
 		std::deque<T> max_list;
 		std::deque<T> min_list;
 		bool n_is_odd;
 	public:
 		PmergeMe();
 		~PmergeMe();
+		bool allDifferent();
 		int checkArg(const std::string& input);
 		int createMaxMinList();
-		bool allDifferent();
 
 		/**
 		 * PRINT / DEBUG
 		 * 
-		 * void printDeque();
 		 * 
 		 */
+		void printDeque(std::deque<T> &list);
 		std::deque<T> getMaxList() const;
 		std::deque<T> getMinList() const;
-		// int assignJacobSthal() const;
 		// const std::deque<T> insertMinList() const;
 };
+
+bool is_not_all_digits(const std::string& str);
+
+template <typename T>
+void jacobInsert(const T& element, std::deque<T>& dest)
+{
+	size_t mid = dest.begin() + (dest.end() - dest.begin()) / 2;
+	T tmp;
+	typename std::deque<T>::iterator it = dest.at(mid);
+	if (element > *it)
+	{
+		while (it != dest.end())
+		{
+			if (element < *it)
+			{
+				dest.insert(it, element);
+				return ;
+			}
+			it++;
+		}
+		dest.insert(std::next(it), element);
+	}
+}
+
+template <typename T>
+void instertMinlist(std::deque<T> min_list, std::deque<T> max_list, std::deque<T> jacob_list)
+{
+	typename std::deque<T>::iterator it = jacob_list.begin();
+	while (it != jacob_list.end())
+	{
+		jacobInsert(min_list.at(*it), max_list);
+		it++;
+	}
+	
+}
+
+template <typename T>
+std::deque<T> generateJacobsthal(int n)
+{
+	std::deque<T> J(n + 1, 0);
+	if (n >= 1)
+		J[1] = 1;
+	for (int i = 2; i <= n; ++i)
+		J[i] = J[i - 1] + 2 * J[i - 2];
+	return J;
+}
 
 template <typename T>
 std::deque<T> PmergeMe<T>::getMaxList() const
@@ -48,27 +94,32 @@ std::deque<T> PmergeMe<T>::getMinList() const
 	return this->min_list;
 }
 
-// template <typename T>
-// void PmergeMe<T>::printDeque()
-// {
-// 	typename std::deque<T>::iterator it;
+template <typename T>
+void PmergeMe<T>::printDeque(std::deque<T> &list)
+{
+	typename std::deque<T>::iterator it;
 
-// 	std::cout << "MAX: " << std::endl;
-// 	for (it = max_list.begin(); it != max_list.end(); it++)
-// 	{
-// 		std::cout << "	" << *it << std::endl;
-// 	}
-// 	std::cout << "min: " << std::endl;
-// 	for (it = min_list.begin(); it != min_list.end(); it++)
-// 	{
-// 		std::cout << "	" << *it << std::endl;
-// 	}
-// 	std::cout << "tokens: " << std::endl;
-// 	for (it = tokens.begin(); it != tokens.end(); it++)
-// 	{
-// 		std::cout << "	" << *it << std::endl;
-// 	}
-// }
+	// std::cout << "MAX: " << std::endl;
+	// for (it = max_list.begin(); it != max_list.end(); it++)
+	// {
+	// 	std::cout << "	" << *it << std::endl;
+	// }
+	// std::cout << "min: " << std::endl;
+	// for (it = min_list.begin(); it != min_list.end(); it++)
+	// {
+	// 	std::cout << "	" << *it << std::endl;
+	// }
+	// std::cout << "tokens: " << std::endl;
+	// for (it = tokens.begin(); it != tokens.end(); it++)
+	// {
+	// 	std::cout << "	" << *it << std::endl;
+	// }
+	std::cout << "sort max list: " << std::endl;
+	for (it = list.begin(); it != list.end(); it++)
+	{
+		std::cout << "	" << *it << std::endl;
+	}
+}
 
 template <typename T>
 int PmergeMe<T>::createMaxMinList()
@@ -145,11 +196,35 @@ int PmergeMe<T>::checkArg(const std::string& input)
 	return 0;
 }
 
-bool is_not_all_digits(const std::string& str);
 
 template <typename T>
 void merge(std::deque<T> &list, int left, int median, int right)
 {
+	int n1 = median - left + 1;
+	int n2 = right - median;
+	int i = -1;
+	int j = -1;
+	int k = left;
+	std::deque<T> tmp_L(n1), tmp_R(n2);
+
+	while (++i < n1)
+		tmp_L[i] = list[left + i];
+	while (++j < n2)
+		tmp_R[j] = list[median + 1 + j];
+	j = 0;
+	i = 0;
+	while (i < n1 && j < n2)
+	{
+		if (tmp_L[i] <= tmp_R[j])
+			list[k] = tmp_L[i++];
+		else
+			list[k] = tmp_R[j++];
+		k++;
+	}
+	while (i < n1)
+		list[k++] = tmp_L[i++];
+	while (j < n2)
+		list[k++] = tmp_R[j++];		
 }
 
 template <typename T>
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -12,9 +12,12 @@ int main (int ac, char *av[])
 	if (pm.checkArg(input))
 		return (std::cerr << "Error: wrong input" << std::endl, 1);
 	pm.createMaxMinList();
-	// pm.printDeque();
 	std::deque<unsigned int> max_list = pm.getMaxList();
+	std::deque<unsigned int> min_list = pm.getMinList();
+	std::deque<unsigned int> jacobSthal = generateJacobsthal<unsigned int>(min_list.size());
+	pm.printDeque(max_list);
 	mergeSort(max_list, 0, max_list.size() - 1);
+	pm.printDeque(max_list);
 	// std::cout << "Before sorting: ";
 	// std::deque<unsigned int>::iterator it;
 	// // for (it = tokens.begin(); it + 1 != tokens.end(); it++)
